6b780a158a0bd61795b3eb607fa6231b5b3335b1
Veka <veka610@gmail.com>
2024-11-15 13:23:05 +0100

    Update workflow-api-parser.ts

--- lib/workflow-api-parser.ts
+++ lib/workflow-api-parser.ts
@@ -41,50 +41,79 @@ export function workflowAPItoViewComfy(source: WorkflowApiJSON): IViewComfyBase 
             }
         }
         try {
-            if (value.class_type === 'CLIPTextEncode') {
-                if (inputs.length > 0) {
-                    const input = inputs[0];
-                    input.valueType = "long-text";
-                    input.title = getTitleFromValue(value.class_type, value);
-                    input.placeholder = getTitleFromValue(value.class_type, value);
+            
+            switch (value.class_type) {
+
+                case 'CLIPTextEncode':
+                    if (inputs.length > 0) {
+                        const input = inputs[0];
+                        input.valueType = "long-text";
+                        input.title = getTitleFromValue(value.class_type, value);
+                        input.placeholder = getTitleFromValue(value.class_type, value);
+                        basicInputs.push({
+                            title: getTitleFromValue(value.class_type, value),
+                            inputs: inputs,
+                            key: `${key}-${value.class_type}`
+                        });
+                    }
+                    break;
+                    
+                case 'RandomNoise':
+                    if (inputs.length > 0) {
+                        const input = inputs[0];
+                        const title = getTitleFromValue(value.class_type, value);
+                        input.valueType = "seed";
+                        input.title = title;
+                        input.placeholder = title;
+                        basicInputs.push({
+                            title: title,
+                            inputs: inputs,
+                            key: `${key}-${value.class_type}`
+                        });
+                    }
+                    break;
+
+                case "LoadImage":
+                case "LoadImageMask":
+                    const uploadInput = inputs.find(input => input.title === "Upload");
+                    if (uploadInput) {
+                        const input = inputs[0];
+                        input.valueType = "image";
+                        input.title = getTitleFromValue(value.class_type, value);
+                        input.placeholder = getTitleFromValue(value.class_type, value);
+                        input.value = null;
+                        basicInputs.push({
+                            title: getTitleFromValue(value.class_type, value),
+                            inputs: [input],
+                            key: `${key}-${value.class_type}`
+                        });
+                    }
+                    break;
+
+                case "VHS_LoadVideo":
+                    const uploadInputIndex = inputs.findIndex(input => input.title === "Video");
+                    if (typeof uploadInputIndex !== "undefined") {
+                        inputs[uploadInputIndex].valueType = "video"
+                        inputs[uploadInputIndex].value = null
+                    }
                     basicInputs.push({
                         title: getTitleFromValue(value.class_type, value),
                         inputs: inputs,
                         key: `${key}-${value.class_type}`
                     });
-                }
-            } else if (value.class_type === "LoadImage" || value.class_type === "LoadImageMask") {
-                const uploadInput = inputs.find(input => input.title === "Upload");
-                if (uploadInput) {
-                    const input = inputs[0];
-                    input.valueType = "image";
-                    input.title = getTitleFromValue(value.class_type, value);
-                    input.placeholder = getTitleFromValue(value.class_type, value);
-                    input.value = null;
-                    basicInputs.push({
-                        title: getTitleFromValue(value.class_type, value),
-                        inputs: [input],
-                        key: `${key}-${value.class_type}`
-                    });
-                }
-            } else if (value.class_type === "VHS_LoadVideo") {
-                const uploadInputIndex = inputs.findIndex(input => input.title === "Video");
-                if (typeof uploadInputIndex !== "undefined") {
-                    inputs[uploadInputIndex].valueType = "video"
-                    inputs[uploadInputIndex].value = null
-                }
-                basicInputs.push({
-                    title: getTitleFromValue(value.class_type, value),
-                    inputs: inputs,
-                    key: `${key}-${value.class_type}`
-                });
-            } else if (inputs.length > 0) {
-                advancedInputs.push({
-                    title: getTitleFromValue(value.class_type, value),
-                    inputs: inputs,
-                    key: `${key}-${value.class_type}`
-                });
+                    break;
+
+                default:
+                    if (inputs.length > 0) {
+                        advancedInputs.push({
+                            title: getTitleFromValue(value.class_type, value),
+                            inputs: inputs,
+                            key: `${key}-${value.class_type}`
+                        });
+                    }
+                    break;
             }
+            
         } catch (e) {
             console.log("Error", e);
         }
@@ -144,7 +173,7 @@ function parseValueType(value: any): InputValueType {
             if (value.toString().indexOf('.') !== -1) {
                 return 'float';
             }
-            return 'string';
+            return 'number';
         case 'bigint':
             return 'bigint';
         case 'boolean':
